ba0a7143d815b5cd4bdae4e3897a0df763a1a831
yangcuiwang <yangcuiwang@qq.com>
2019-09-27 00:21:50 +0800

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "go-cms/beeapi/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
