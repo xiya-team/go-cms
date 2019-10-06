feedd2d4a53d48c384f12acd6251fb5d2f0d5663
yangcuiwang <yangcuiwang@qq.com>
2019-10-06 10:52:02 +0800

    快速运行

--- run.sh
+++ run.sh
@@ -3,19 +3,19 @@
 case $1 in 
 	start)
 	    go build
-		nohup ./go_cms 2>&1 >> project.log 2>&1 /dev/null &
+		nohup ./go-cms 2>&1 >> project.log 2>&1 /dev/null &
 		echo "服务已启动..."
 		sleep 1
 	;;
 	stop)
-		killall go_cms
+		killall go-cms
 		echo "服务已停止..."
 		sleep 1
 	;;
 	restart)
-		killall go_cms
+		killall go-cms
 		sleep 1
-		nohup ./go_cms 2>&1 >> project.log 2>&1 /dev/null &
+		nohup ./go-cms 2>&1 >> project.log 2>&1 /dev/null &
 		echo "服务已重启..."
 		sleep 1
 	;;
