e6e266137faaacfefe2ceec2a39d173cb34a1038
yangcuiwang <yangcuiwang@qq.com>
2019-10-14 10:54:50 +0800

    url调整

--- routers/router.go
+++ routers/router.go
@@ -15,6 +15,7 @@ func init() {
 	//)
 	//beego.AddNamespace(ns)
 
+	//用户相关
 	beego.Router("/api/user/login", &sys.UserController{}, "post:Login")
 	beego.Router("/api/user/create", &sys.UserController{}, "post:Create")
 	beego.Router("/api/user/info", &sys.UserController{}, "get:UserInfo") // 获取用户消息
@@ -22,6 +23,7 @@ func init() {
 	beego.Router("/api/user/check_token", &sys.UserController{}, "post:CheckToken")
 	beego.Router("/api/user/logout", &sys.UserController{}, "get:Logout")
 	
+	//验证码校验
 	beego.Router("/api/captcha/check", &sys.CaptchaController{}, "post:Hander")
 	
 	//参数设置
@@ -30,13 +32,11 @@ func init() {
 	beego.Router("/api/configs/update", &sys.ConfigsController{}, "post:Update")
 	beego.Router("/api/configs/delete", &sys.ConfigsController{}, "post:Delete")
 	
-	
 	//岗位管理
 	beego.Router("/api/post/index", &sys.PostController{}, "post:Index")
 	beego.Router("/api/post/create", &sys.PostController{}, "post:Create")
 	beego.Router("/api/post/update", &sys.PostController{}, "post:Update")
 	beego.Router("/api/post/delete", &sys.PostController{}, "post:Delete")
-	
 	
 	//菜单管理
 	beego.Router("/api/menu/index", &sys.MenuController{}, "post:Index")
