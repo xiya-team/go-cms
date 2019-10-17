7753fe4e0407a08da0f00a016991b2bdba2ecc5f
ginkgohat <ginkgohat@gmail.com>
2019-10-17 15:46:20 +0800

    update  page struct

--- pkg/util/pagination.go
+++ pkg/util/pagination.go
@@ -1,32 +1,41 @@
 package util
 
+// PageData ...
 type PageData struct {
-	Page Page
-	List       interface{}
+	Page Page        `json:"page"`
+	List interface{} `json:"list"`
 }
 
+// Page ...
 type Page struct {
-	PageNo     int64
-	PageSize   int64
-	TotalPage  int64
-	TotalCount int64
-	FirstPage  bool
-	LastPage   bool
+	PageNo     int64 `json:"page_no"`
+	PageSize   int64 `json:"page_size"`
+	TotalPage  int64 `json:"tatal_page"`
+	TotalCount int64 `json:"tatal_count"`
+	FirstPage  bool  `json:"first_page"`
+	LastPage   bool  `json:"last_page"`
 }
 
+// PageUtil ...
 func PageUtil(count int64, pageNo int64, pageSize int64, list interface{}) PageData {
-	tp := count / pageSize
-	if count % pageSize > 0 {
-		tp = count / pageSize + 1
+
+	if pageNo <= 0 {
+		pageNo = 1
 	}
-	
-	page := Page{}
-	page.PageNo = pageNo
-	page.PageSize = pageSize
-	page.TotalPage = tp
-	page.TotalCount = count
-	page.FirstPage = pageNo == 1
-	page.LastPage = pageNo == tp
-	
-	return PageData{Page:page, List: list}
+
+	tp := count / pageSize
+	if count%pageSize > 0 {
+		tp = count/pageSize + 1
+	}
+
+	page := Page{
+		PageNo:     pageNo,
+		PageSize:   pageSize,
+		TotalPage:  tp,
+		TotalCount: count,
+		FirstPage:  pageNo == 1,
+		LastPage:   pageNo == tp,
+	}
+
+	return PageData{Page: page, List: list}
 }
