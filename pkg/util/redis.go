c258abaeeece09330aa37734208e0060c3a23566
yangcuiwang <yangcuiwang@qq.com>
2019-11-19 09:03:57 +0800

    单例模式 实现redis连接

--- pkg/util/redis.go
+++ pkg/util/redis.go
@@ -3,16 +3,28 @@ package util
 import (
 	"github.com/astaxie/beego"
 	"github.com/go-redis/redis/v7"
+	"sync"
 )
 
+var (
+	once sync.Once
+	redisClient *redis.Client
+)
+
+/**
+ * 单例模式 实现redis连接
+ */
 func NewRedisClient() (*redis.Client,error) {
-	index,_:=  beego.AppConfig.Int("redis::index")
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:     	beego.AppConfig.String("redis::addr"),
-		Password: 	beego.AppConfig.String("redis::password"),
-		DB:       	index,
-		PoolSize:	10,
+	once.Do(func() {
+		index,_:=  beego.AppConfig.Int("redis::index")
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     	beego.AppConfig.String("redis::addr"),
+			Password: 	beego.AppConfig.String("redis::password"),
+			DB:       	index,
+			PoolSize:	10,
+		})
 	})
+
 	_,err := redisClient.Ping().Result()
 	if err!=nil {
 		return nil,err
